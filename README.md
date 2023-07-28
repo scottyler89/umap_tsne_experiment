9d8dac1a717469e10c32f51d9973f428fad4fae7
scottyler892 <scottyler89+bitbucket@gmail.com>
2023-07-28 13:57:41 -0400

    finished first draft of full analysis

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# umap_tsne_experiment
-Testing if UMAP/tSNE overfit their intrinsic dimensionality
+# Do tSNE and UMAP overfit their intrinsic dimensionality (Yes)
+
+This repository presents an analysis of the intrinsic dimensionality of data that has true dimensions and additional noise dimensions. The primary goal is to explore how various noise levels influence the perceived dimensionality and how different dimensionality reduction methods represent the data.
+
+## Intention
+
+The main intention behind this analysis is to:
+1. Generate synthetic data with known "true dimensions" and introduce redundant noisy dimensions.
+2. Observe how different dimension reduction techniques, namely t-SNE and UMAP, represent the data with increasing noise.
+3. Estimate the intrinsic dimensionality as noise levels increase and analyze its implications.
+
+## Methods Walkthrough
+
+1. **Data Generation**: 
+   - We started by creating synthetic data with known dimensions using a random number generator.
+   - For each true dimension, redundant dimensions were added. These dimensions were seeded from the true data but had noise introduced, which was controlled by a specified standard deviation ratio (`sd_ratio`).
+
+2. **Dimension Reduction**:
+   - For each noise level, dimensionality was reduced using two popular methods: t-SNE and UMAP.
+   - The reduced dimensions were then visualized against the true dimensions to observe the effects of noise.
+
+3. **Intrinsic Dimensionality Estimation**:
+   - At each noise level, the intrinsic dimensionality of the data was estimated using the DANCo method.
+   - This helped us understand how perceived dimensionality changes as noise increases.
+
+## Results
+
+![True Dimensions vs Dimension Reduction](assets/true_dims_with_noise_vs_dim_reduction.png)
+
+From the above figure, we can observe how t-SNE and UMAP represent the data across different noise levels. As noise increases, the clarity of clusters in the reduced space might get affected.
+
+![Heatmap and Scatters](assets/heatmap_and_scatters.png)
+
+This visualization provides a heatmap of observed data against the ground truth data. The scatter plots showcase the relationship between the noisy dimensions and the original data. A red line of best fit is added for clarity.
+
+![Intrinsic Dimensions Increase with Noise](assets/intrinsic_dims_increase_with_noise.png)
+
+This plot highlights an interesting phenomenon. As noise levels (or the `sd_ratio`) increase, the estimated intrinsic dimensionality also rises. This resonates with the notion that added noise in one dimension is perceived as adding its own dimension. The challenge lies in deciphering which dimensions are "meaningful" and which are mere noise.
+
+## Conclusion
+
+Noise, when added to data, can complicate the structure and perceived dimensionality of the dataset. While dimensionality reduction techniques like t-SNE and UMAP strive to represent this complexity, it's crucial to understand the source of the data and the nature of the noise when interpreting the results.
+
+## References
+
+- [DANCo: Dimensionality from Angle and Norm Concentration. Camastra & Vinciarelli, 2012](https://doi.org/10.48550/arXiv.1206.3881)
--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
-
+import skdim
+import seaborn as sns
 import matplotlib.pyplot as plt
 import umap
 import numpy as np
@@ -22,31 +23,23 @@ def generate_data(n_obs, true_dims, n_redundant_per_true, true_gen_func, redunda
     - main_mat (numpy matrix): Matrix of true dimensions
     - redundant_mat (numpy matrix): Matrix of redundant dimensions
     """
-
     # Generate the main matrix
     main_mat = true_gen_func(n_obs, true_dims)
-
     # Placeholder for the redundant dimensions
     redundant_dims = []
-
     for i in range(true_dims):
         # Standard deviation for this dimension in main_mat
         dim_std = np.std(main_mat[:, i])
-
         # Create n_redundant_per_true redundant dimensions seeded at main_mat[:, i] values
         for _ in range(n_redundant_per_true):
             noise = redundant_gen_noise_func(n_obs, 1)
             redundant_dim = main_mat[:, i][:,
                                            np.newaxis] + noise * sd_ratio * dim_std
-
             # Standardize the redundant dimension
             redundant_dim = redundant_dim / np.std(redundant_dim)
-
             redundant_dims.append(redundant_dim)
-
     # Stack all redundant dimensions horizontally
     redundant_mat = np.hstack(redundant_dims)
-
     return main_mat, redundant_mat
 
 
@@ -62,43 +55,20 @@ def dim_reduction(in_mat, dim_red_func_list, dim_red_names, final_dims):
     Returns:
     - results (dict): dictionary of dim_red_names and their results
     """
-    
     # Placeholder for the results from each dimension reduction function
     results = {}
-    
     for func, name in zip(dim_red_func_list, dim_red_names):
         result = func(in_mat, final_dims)
         results[name]=result
-        
     return results
 
 
 
 def tsne_wrapper(data, n_components):
-    """
-    Wrapper for t-SNE.
-    
-    Args:
-    - data (numpy array): Input data matrix
-    - n_components (int): Number of dimensions after reduction
-    
-    Returns:
-    - Reduced data (numpy array)
-    """
     tsne = TSNE(n_components=n_components)
     return tsne.fit_transform(data)
 
 def umap_wrapper(data, n_components):
-    """
-    Wrapper for UMAP.
-    
-    Args:
-    - data (numpy array): Input data matrix
-    - n_components (int): Number of dimensions after reduction
-    
-    Returns:
-    - Reduced data (numpy array)
-    """
     reducer = umap.UMAP(n_components=n_components)
     return reducer.fit_transform(data)
 
@@ -133,7 +103,6 @@ def redundant_gen_noise_func(n_obs, true_dims):
 
 #########################################
 
-
 def plot_dim_reductions(true_dim_dict, results_dict):
     """
     Plots scatter plots of true dimensions and results of dimension reduction methods.
@@ -152,25 +121,142 @@ def plot_dim_reductions(true_dim_dict, results_dict):
     # Create a figure with subplots
     fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5 * n_rows))
 
+    # Adjust the spacing between subplots
+    # Adjust these values as needed for desired spacing
+    fig.subplots_adjust(wspace=0.3, hspace=0.3)
+
     # Loop through each sd_ratio and plot
     for i, sd_ratio in enumerate(results_dict):
+        # Row title for sd_ratio
+        if n_cols > 1:
+            axes[i, 0].set_ylabel(sd_ratio, fontsize=28,
+                                  rotation=90, labelpad=50, va="center")
+        else:
+            axes.set_ylabel(sd_ratio, fontsize=28, rotation=90,
+                            labelpad=50, va="center")
+
         # Plot true dimensions
         axes[i, 0].scatter(true_dim_dict[sd_ratio][:, 0],
-                           true_dim_dict[sd_ratio][:, 1], alpha=0.6)
-        axes[i, 0].set_title(f"True Dimensions ({sd_ratio})")
+                           true_dim_dict[sd_ratio][:, 1], alpha=0.6, s=5)
+        #axes[i, 0].set_title(f"True Dimensions", fontsize=12)
 
         # Loop through each dimension reduction method and plot
         for j, method in enumerate(results_dict[sd_ratio]):
             axes[i, j+1].scatter(results_dict[sd_ratio][method][:, 0],
-                                 results_dict[sd_ratio][method][:, 1], alpha=0.6)
-            axes[i, j+1].set_title(f"{method} ({sd_ratio})")
+                                 results_dict[sd_ratio][method][:, 1], alpha=0.6, s=5)
+            #axes[i, j+1].set_title(f"{method}", fontsize=12)
 
+    # Add column titles
+    col_titles = ['Intrinsic Dimensions\nthat created\ninput values+noise'] + \
+        list(next(iter(results_dict.values())).keys())
+    for ax, col in zip(axes[0], col_titles):
+        ax.annotate(col, (0.5, 1.15), xycoords='axes fraction', ha='center',
+                    va='center', fontsize=28, textcoords='offset points')
+    for ax_row in axes:
+        for ax in ax_row:
+            for spine in ax.spines.values():
+                spine.set_linewidth(2)
+    plt.savefig("assets/true_dims_with_noise_vs_dim_reduction.png", dpi=300)
+
+
+def rotate_point_around_origin(point, angle):
+    """
+    Rotates a point around the origin by a given angle.
+    
+    Args:
+    - point (tuple): (x, y) coordinates of the point
+    - angle (float): Angle in radians to rotate
+    
+    Returns:
+    - (x', y'): Rotated coordinates of the point
+    """
+    x, y = point
+    x_rot = x * np.cos(angle) - y * np.sin(angle)
+    y_rot = x * np.sin(angle) + y * np.cos(angle)
+    return x_rot, y_rot
+
+
+def plot_obs_data_heatmap(gt_data_dict, obs_data_dict, danco_dict):
+    """
+    Plots heatmaps of obs_data for each sd_ratio.
+    
+    Args:
+    - gt_data_dict (dict): Dictionary of ground truth data matrices for each sd_ratio
+    - obs_data_dict (dict): Dictionary of obs_data matrices for each sd_ratio
+    
+    """
+    # Number of rows is the number of sd_ratios
+    n_rows = len(obs_data_dict)
+
+    # Create a figure with subplots
+    fig, axes = plt.subplots(n_rows, 2, figsize=(10, 5 * n_rows))
+    fig.subplots_adjust(wspace=0.35, hspace=0.38)
+
+    if n_rows == 1:
+        axes = [axes]
+
+    # Loop through each sd_ratio and plot
+    for i, sd_ratio in enumerate(obs_data_dict.keys()):
+        # Extract ground truth and observation data for scatter plot
+        temp_gt_data = gt_data_dict[sd_ratio]
+        temp_obs_data = obs_data_dict[sd_ratio]
+        estimated_dims = danco_dict[sd_ratio]
+
+        # Plot the heatmap
+        sns.heatmap(temp_obs_data, ax=axes[i, 0], cmap="YlGnBu", cbar=False)
+        axes[i, 0].set_title(f"DANCo dim\nestimate {estimated_dims:.2f}", fontsize=23)
+        axes[i, 0].set_xticks([])
+        axes[i, 0].set_yticks([])
+        axes[i, 0].set_ylabel(sd_ratio, fontsize=26, rotation=90,
+                              labelpad=50, va="center")
+
+        # Scatter plot
+        axes[i, 1].scatter(temp_obs_data[:, 0], temp_gt_data[:, 0], alpha=0.5)
+        #axes[i, 1].set_title(f"Scatter for {sd_ratio}", fontsize=14)
+        axes[i, 1].set_xlabel("Dim-1 Redundant + noise", fontsize=23)
+        axes[i, 1].set_ylabel("Ground-truth Dim-1", fontsize=23)
+        for spine in plt.gca().spines.values():
+            spine.set_linewidth(2)
+
+        # Add correlation line
+        m, b = np.polyfit(temp_obs_data[:, 0], temp_gt_data[:, 0], 1)
+        axes[i, 1].plot(temp_obs_data[:, 0], m *
+                        temp_obs_data[:, 0] + b, color='red', linewidth=2)
+    for ax_row in axes:
+        for ax in ax_row:
+            for spine in ax.spines.values():
+                spine.set_linewidth(2)
+    plt.savefig("assets/heatmap_and_scatters.png", dpi=300)
+
+
+#########
+
+
+def plot_intrinsic_dimensionality(sd_lookup, intrinsic_dim_estimate_dict):
+    """
+    Plots the estimated intrinsic dimensionality against noise levels.
+    
+    Args:
+    - sd_lookup (dict): Dictionary mapping sd_ratio names to their respective values
+    - intrinsic_dim_estimate_dict (dict): Dictionary of estimated intrinsic dimensionality for each sd_ratio
+    """
+    # Extract data
+    sd_values = [sd_lookup[key] for key in intrinsic_dim_estimate_dict.keys()]
+    dim_estimates = list(intrinsic_dim_estimate_dict.values())
+
+    # Create a scatter plot with loess fit curve
+    plt.figure(figsize=(10, 6))
+    sns.regplot(x=sd_values, y=dim_estimates, lowess=True, scatter_kws={
+                's': 100, 'alpha': 0.6}, line_kws={'color': 'red', 'lw': 2})
+    plt.xlabel("Noise Level (SD Ratio)")
+    plt.ylabel("Estimated Intrinsic Dimensionality")
+    plt.title("Intrinsic Dimensionality vs. Noise Level")
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
     plt.tight_layout()
-    plt.show()
+    plt.savefig("assets/intrinsic_dims_increase_with_noise.png", dpi=300)
 
 
-
-
+#########
 #########################################
 
 # Parameters for the experiment
@@ -178,17 +264,34 @@ np.random.seed(123456)
 n_obs = 1000
 true_dims = 2
 n_redundant_per_true = 100
-sd_ratios = [0.01, 0.05, 0.25]
+sd_ratios = [0.01, 0.05, 0.25, 0.5, 1.]
+intrinsic_dim_estimate_dict = {}
 true_dim_dict = {}
+obs_data_dict = {}
 results_dict = {}
+sd_lookup = {}
 for sd_ratio in sd_ratios:
     sd_name = "SD ratio:"+str(sd_ratio)
+    sd_lookup[sd_name] = sd_ratio
     final_dims = true_dims  # This is just an example; adjust as needed
     # Generate data
     true_dim_data, obs_data = generate_data(n_obs, true_dims, n_redundant_per_true, true_gen_func, redundant_gen_noise_func, sd_ratio)
 
-    # log the true dimensions
+    # Estimates of intrinsic dimensionality.
+    # Interesting note here, but it actually identifies
+    # that as noise dimensions are added, and the size of the noise relative to
+    #  dims are 'real dims.' This fits with the model of it finding 
+    # that added noise in one dimension is actually adding its own dimension, even if the 'real'
+    # varyation was already accounted for by prior dims. It's not like this is incorrect or anything...
+    # It's just that noise is a dimension. The hard part is figuring out which dims are "meaningful"!
+    ## https: // doi.org/10.48550/arXiv.1206.3881
+    danco = skdim.id.DANCo().fit(obs_data)
+    print(danco.dimension_)
+    intrinsic_dim_estimate_dict[sd_name] = danco.dimension_
+
+    # log the data
     true_dim_dict[sd_name] = true_dim_data
+    obs_data_dict[sd_name] = obs_data
 
     # Perform dimension reduction
     dim_red_funcs = [tsne_wrapper, umap_wrapper]
@@ -199,4 +302,13 @@ for sd_ratio in sd_ratios:
 # Call the function to plot
 plot_dim_reductions(true_dim_dict, results_dict)
 
+
+
+# Call the function to plot
+plot_obs_data_heatmap(true_dim_dict, obs_data_dict,
+                      intrinsic_dim_estimate_dict)
+
+
+# Call the function to plot
+plot_intrinsic_dimensionality(sd_lookup, intrinsic_dim_estimate_dict)
 
